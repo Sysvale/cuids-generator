6a25e4ee2461222d356f02e61a0d912ae05f9626
Igor Vinicius <igorvinimec@gmail.com>
2025-12-19 14:51:17 -0300

    feat: Adiciona novos pos processadores

---
+++ src/Blueprint/PostProcessors/ControllerPostProcessor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sysvale\CuidsGenerator\Blueprint\PostProcessors;
+
+use Illuminate\Support\Facades\File;
+
+class ControllerPostProcessor
+{
+    public function handle(string $model): void
+    {
+        $path = base_path("app/Http/Controllers/{$model}Controller.php");
+
+        if (! File::exists($path)) return;
+
+        $content = File::get($path);
+        $content = preg_replace(
+            
+            '/(Request\s+\$request,?\s*)|(Request\s+\$request\s*$)/',
+            '',
+            $content
+        );
+
+        File::put($path, $content);
+    }
+}
