c0d9db8cab1fa94a386c2edf93bb3a091881221f
Igor Vinicius <igorvinimec@gmail.com>
2025-12-19 11:38:27 -0300

    feat: Adiciona novos pos processadores no service provider

--- src/Providers/CuidsGeneratorServiceProvider.php
+++ src/Providers/CuidsGeneratorServiceProvider.php
@@ -2,20 +2,23 @@
 
 namespace Sysvale\CuidsGenerator\Providers;
 
-use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 use Sysvale\CuidsGenerator\Console\Commands\CuidsGenerateCommand;
 use Sysvale\CuidsGenerator\Blueprint\PostProcessorRunner;
+use Sysvale\CuidsGenerator\Blueprint\PostProcessors\CollectionPostProcessor;
 use Sysvale\CuidsGenerator\Blueprint\PostProcessors\FactoryPostProcessor;
 use Sysvale\CuidsGenerator\Blueprint\PostProcessors\Mongo\MongoModelTransformer;
 use Sysvale\CuidsGenerator\Blueprint\PostProcessors\RequestPostProcessor;
 use Sysvale\CuidsGenerator\Blueprint\PostProcessors\TestPostProcessor;
+use Illuminate\Support\Facades\File;
+use Sysvale\CuidsGenerator\Blueprint\PostProcessors\ControllerPostProcessor;
 
 class CuidsGeneratorServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->registerPostProcessors();
+        $this->configureBlueprintStubs();
 
         if ($this->app->runningInConsole()) {
             $this->commands([
@@ -32,8 +35,30 @@ class CuidsGeneratorServiceProvider extends ServiceProvider
                 $app->make(FactoryPostProcessor::class),
                 $app->make(RequestPostProcessor::class),
                 $app->make(TestPostProcessor::class),
+                $app->make(CollectionPostProcessor::class),
+                $app->make(ControllerPostProcessor::class),
             ]);
         });
+    }
+
+    private function configureBlueprintStubs(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $targetPath = base_path('stubs/blueprint');
+            $sourcePath = realpath(__DIR__ . '/../../stubs/blueprint');
+
+            if ($sourcePath && !file_exists($targetPath)) {
+                if (!is_dir(base_path('stubs'))) {
+                    mkdir(base_path('stubs'), 0755, true);
+                }
+
+                if (function_exists('symlink')) {
+                    symlink($sourcePath, $targetPath);
+                } else {
+                    File::copyDirectory($sourcePath, $targetPath);
+                }
+            }
+        }
     }
 
     public function boot(): void
