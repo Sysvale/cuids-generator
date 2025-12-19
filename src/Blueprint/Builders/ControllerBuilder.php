b7d8ad6bf1fe232adb814b11c0e3753b4f328e5e
Igor Vinicius <igorvinimec@gmail.com>
2025-12-19 14:52:35 -0300

    refactor: Realiza ajuste no controllerBuilder para resposta dos metodos

--- src/Blueprint/Builders/ControllerBuilder.php
+++ src/Blueprint/Builders/ControllerBuilder.php
@@ -20,7 +20,7 @@ class ControllerBuilder
                 'store' => [
                     'validate' => "Store{$model}Request",
                     'save' => $singularVar,
-                    'respond' => 201,
+                    'resource' => $singularVar,
                 ],
                 'show' => [
                     'resource' => $singularVar,
@@ -28,11 +28,11 @@ class ControllerBuilder
                 'update' => [
                     'validate' => "Update{$model}Request",
                     'update' => $singularVar,
-                    'respond' => 200
+                    'resource' => $singularVar,
                 ],
                 'destroy' => [
                     'delete' => $singularVar,
-                    'respond' => 204
+                    'respond' => 204,
                 ],
             ],
         ];
